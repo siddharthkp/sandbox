9f36e9cae5fe1cad3c0bd7b965aefe5ae0c0434a
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2016-09-24 14:42:02 +0530

    Basic setup

---
+++ index.js
@@ -0,0 +1,17 @@
+require('babel-register');
+
+const express = require('express');
+const React = require('react');
+const ReactDOM = require('react-dom/server');
+const pug = require('pug');
+
+const app = express();
+app.set('view engine', 'pug');
+app.use(express.static(__dirname + '/static'));
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.listen(8080);
+
---
+++ static/scripts/list.jsx
@@ -0,0 +1,10 @@
+let List = React.createClass({
+    render: () => {
+        return <div>sup</div>;
+    }
+});
+
+ReactDOM.render(
+    <List/>,
+    document.getElementById('container')
+);
